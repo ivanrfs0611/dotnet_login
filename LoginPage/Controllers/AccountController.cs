c30b3ec4dec1fa293542fb3cba8344152321bccd
Ivan Rafi Susanto <susantoivan0611@gmail.com>
2021-10-06 16:02:03 +0700

    * .gitignore: first init
    * LoginPage.sln:
    * Program.cs:
    * Startup.cs:
    * LoginPage.csproj:
    * appsettings.json:
    * Account.cs:
    * site.js:
    * Index.cshtml:
    * favicon.ico:
    * site.css:
    * style.css:
    * _ViewStart.cshtml:
    * Index.cshtml:
    * ErrorViewModel.cs:
    * _ViewImports.cshtml:
    * fontawesome.js:
    * _LayoutPage1.cshtml:
    * Error.cshtml:
    * Privacy.cshtml:
    * Login.cshtml:
    * Failed.cshtml:
    * Create.cshtml:
    * _Layout.cshtml:
    * _LayoutPage1.cshtml.cs:
    * appsettings.Development.json:
    * Login.cshtml.cs:
    * HomeController.cs:
    * LICENSE:
    * launchSettings.json:
    * LICENSE.txt:
    * AccountController.cs:
    * jquery.js:
    * jquery.min.js:
    * jquery.min.map:
    * LICENSE.md:
    * bootstrap.js:
    * bootstrap.css:
    * _ValidationScriptsPartial.cshtml:
    * bootstrap.js.map:
    * bootstrap.min.js:
    * bootstrap.css.map:
    * bootstrap.min.css:
    * bootstrap.bundle.js:
    * bootstrap-grid.css:
    * bootstrap.min.js.map:
    * bootstrap-reboot.css:
    * bootstrap.min.css.map:
    * bootstrap.bundle.js.map:
    * bootstrap.bundle.min.js:
    * bootstrap-grid.css.map:
    * bootstrap-grid.min.css:
    * LICENSE.txt:
    * jquery.validate.js:
    * bootstrap-reboot.min.css:
    * bootstrap-reboot.css.map:
    * additional-methods.js:
    * jquery.validate.min.js:
    * bootstrap-grid.min.css.map:
    * bootstrap.bundle.min.js.map:
    * bootstrap-reboot.min.css.map:
    * additional-methods.min.js:
    * jquery.validate.unobtrusive.js:
    * jquery.validate.unobtrusive.min.js:

---
+++ LoginPage/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LoginPage.Models;
+using MySql.Data.MySqlClient;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LoginPage.Controllers
+{
+    public class AccountController : Controller
+    {
+        MySqlConnection con = new MySqlConnection();
+        MySqlCommand cmd = new MySqlCommand();
+        MySqlDataReader dr;
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        void connectionString()
+        {
+            con.ConnectionString = "server=localhost;database=dotnet;user=root;port=8889;password=root";
+        }
+        [HttpPost]
+        public ActionResult Verify(Account acc)
+        {
+            connectionString();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "select * from Person where Email = '"+acc.Email+"' and Password = '"+acc.Password+"'";
+            dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                return View("Create");
+            }
+            else
+            {
+                con.Close();
+                return View("Failed");
+            }
+        }
+    }
+}
---
+++ LoginPage/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace LoginPage.Models
+{
+    public class Account
+    {
+       public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
